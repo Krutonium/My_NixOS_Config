5503403b262c4c516fbbe573f9fd74db5175201c
Krutonium <PFCKrutonium@gmail.com>
2021-12-25 07:19:00 -0500

    Moved Satisfactory to a Service

--- Servers/Satisfactory/Run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-nix-shell --pure \
-          -p "steam-run-native" \
-          --run "steam-run ./SatisfactoryDedicatedServer/FactoryServer.sh -NOSTEAM"
--- Servers/Satisfactory/Update.sh
+++
@@ -1,1 +0,0 @@
-nix-shell -p steamcmd --run "steamcmd +force_install_dir /home/krutonium/FactoryGame/SatisfactoryDedicatedServer/ +login anonymous +app_update 1690800 validate +quit"
